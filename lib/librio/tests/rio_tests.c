06475486d63cf7e47d8e13c74d3ef282629d8898
Ryan Moeller <rmoeller.dev@gmail.com>
2026-01-24 01:48:22 -0500

    WIP: librio: Add saturation test
    Excercise blocking on various queues.

--- lib/librio/tests/rio_tests.c
+++ lib/librio/tests/rio_tests.c
@@ -18,7 +18,7 @@
 /* ring sizes (must be power of 2) */
 #define SQLEN	32
 #define CQLEN	32
-#define NCB	64
+#define NCB	128	/* > SQLEN + CQLEN */
 #define POLICY	0
 #define TESTCONFIG	SQLEN, CQLEN, NCB, POLICY
 
@@ -186,11 +186,77 @@ ATF_TC_BODY(errors, tc)
 	rio_destroy(rio);
 }
 
+ATF_TC(saturation);
+ATF_TC_HEAD(saturation, tc)
+{
+	atf_tc_set_md_var(tc, "descr", "Tests full ring blocking");
+}
+
+ATF_TC_BODY(saturation, tc)
+{
+	const int limit = 1000;
+	const struct timespec deadline = {0};
+	struct riocb iocb = {0};
+	rio_t rio = NULL;
+	int fd, s, c;
+
+	ATF_REQUIRE(rio_create(&rio, TESTCONFIG) == 0);
+	ATF_REQUIRE((fd = open("testfile", O_CREAT | O_RDWR, 0444)) != -1);
+
+	/* Write a string to the file. */
+	iocb.rio_ident = fd;
+	iocb.rio_buf = __DECONST(char *, "test");
+	iocb.rio_length = 4;
+	/* TODO: Test with multiple threads and different qlens/ncb. */
+	s = c = 0;
+	while (s < limit) {
+		/* Non-blocking enqueue, submit when full. */
+		while (rio_write(rio, &iocb, &deadline) == 0) {
+			s++;
+		}
+		ATF_CHECK_INTEQ(ETIMEDOUT, errno);
+		/* Submission queue full. */
+		ATF_CHECK(rio_submit(rio) == 0);
+		/* Blocking enqueue, submit when full again. */
+		for (int i = 0; i < SQLEN - 1; i++) {
+			ATF_CHECK(rio_fsync(rio, &iocb, NULL) == 0);
+			s++;
+		}
+		ATF_CHECK(rio_submit(rio) == 0);
+		/*
+		 * Fill faster than we drain, which the kernel should handle
+		 * (barring policy).  The workers cannot complete, so their
+		 * queues begin growing backlogs while the issuers continue to
+		 * drain submissions.
+		 *
+		 * s - c = # in-flight, we want to block workers with a full
+		 * completion queue (CQLEN - 1).
+		 */
+		while (s - c > CQLEN - 1) {
+			ATF_CHECK(rio_poll(rio, &iocb, NULL) == 0);
+			ATF_CHECK_INTEQ(iocb.rio_ident, fd);
+			ATF_CHECK_INTEQ(iocb.rio_error, 0);
+			c++;
+		}
+	}
+	/* Drain remaining completions. */
+	while (s > c) {
+		ATF_CHECK(rio_poll(rio, &iocb, NULL) == 0);
+		ATF_CHECK_INTEQ(iocb.rio_ident, fd);
+		ATF_CHECK_INTEQ(iocb.rio_error, 0);
+		c++;
+	}
+
+	ATF_CHECK(close(fd) == 0);
+	rio_destroy(rio);
+}
+
 ATF_TP_ADD_TCS(tp)
 {
 	ATF_TP_ADD_TC(tp, create_destroy);
 	ATF_TP_ADD_TC(tp, polling);
 	ATF_TP_ADD_TC(tp, vectors);
 	ATF_TP_ADD_TC(tp, errors);
+	ATF_TP_ADD_TC(tp, saturation);
 	return (atf_no_error());
 }
