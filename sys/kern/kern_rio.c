9a8662b25e4b52107327a2b82875daa248a11364
Ryan Moeller <rmoeller.dev@gmail.com>
2026-01-23 19:03:12 -0500

    WIP: rio: Group issuer threads into a rio kproc

--- sys/kern/kern_rio.c
+++ sys/kern/kern_rio.c
@@ -1351,6 +1351,8 @@ rio_worker_proc(void *arg)
 	kproc_exit(0);
 }
 
+static struct proc *rio_issuers;
+
 static inline int
 rio_issuer_init(struct rio_issuer *issuer, u_int cpu)
 {
@@ -1364,10 +1366,11 @@ rio_issuer_init(struct rio_issuer *issuer, u_int cpu)
 	/* TODO: automatic startup/shutdown (kick taskqueue?) */
 	for (u_int i = 0; i < rio_flow_issuer_threads; i++) {
 		/* Spawn issuer threads in the proc0 kernel process. */
-		if ((error = kthread_add(rio_issuer_thread, issuer, NULL, NULL,
-		    0, 0, "rio/issue%u.%u", cpu, i)) != 0) {
+		if ((error = kproc_kthread_add(rio_issuer_thread, issuer,
+		    &rio_issuers, NULL, 0, 0, "rio", "issue %u.%u", cpu, i))
+		    != 0) {
 			/* TODO: error handling */
-			printf("%s: kthread_add: %d\n", __func__, error);
+			printf("%s: kproc_kthread_add: %d\n", __func__, error);
 			return (error);
 		}
 		issuer->ri_threads++;
@@ -1389,7 +1392,7 @@ rio_worker_init(struct rio_worker *worker, rio_srcio_handler_f *handler,
 	STAILQ_INIT(&worker->rw_srcios);
 	/* Spawn each worker as its own kernel process. */
 	if ((error = kproc_create(rio_worker_proc, worker, NULL, 0, 0,
-	    "rio/%s%u.%u", classname, cpu, i)) != 0) {
+	    "rio/%s %u.%u", classname, cpu, i)) != 0) {
 		/* TODO: error handling */
 		printf("%s: kproc_create: %d\n", __func__, error);
 		return (error);
